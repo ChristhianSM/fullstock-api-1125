a84e725688cd0d1172b9a7a153c4b45289cfab51
ChristhianSM <christhian2524@gmail.com>
2026-04-29 20:19:55 -0500

    agregamos arquitectura por capas

---
+++ src/repositories/category.repository.ts
@@ -0,0 +1,20 @@
+import camelCaseKeys from "camelcase-keys";
+import * as db from "../db/index.ts";
+
+interface CategoryRow {
+  id: number;
+  title: string;
+  slug: string;
+  img_src: string;
+  alt: string | null;
+  description: string;
+  create_at: Date;
+  update_at: Date;
+}
+
+export type Category = ReturnType<typeof camelCaseKeys<CategoryRow>>;
+
+export async function getAll(): Promise<Category[]> {
+  const result = await db.query<CategoryRow>("SELECT * FROM categories");
+  return camelCaseKeys(result.rows);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import app from "./app.ts";
+// import * as db from "./db/index.ts";
+import * as db from "./db/index.ts";
 
 const PORT = process.env["PORT"] ?? 3000;
+
+try {
+  await db.query("SELECT 1");
+} catch (error) {
+  console.error("Ocurrio un problema con la conexion:", error);
+  process.exit(1);
+}
 
 app.listen(PORT, () => {
   console.log("El servidor corriendo en el puerto: ", PORT);
