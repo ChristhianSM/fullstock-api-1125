e4a50979b1c1572d311a7ce730d3bbd7660c7cb1
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2026-05-05 01:19:30 -0500

    Agrega rutas /api/products/:slug para obtener detalle del producto segun su slug

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { ApiError } from "../lib/errors.ts";
+import * as productRepository from "../repositories/product.repository.ts";
 import * as categoryService from "../services/category.service.ts";
 import * as productService from "../services/product.service.ts";
 import * as categoryController from "./category.controller.ts";
@@ -18,4 +19,20 @@ export async function getProductsByCategory(
   const products = await productService.getProductsByCategory(slug);
 
   res.json({ status: "success", data: products });
+}
+
+export interface ProductSlugParams {
+  slug: productRepository.ProductSlug;
+}
+
+export async function getProduct(
+  req: Request<ProductSlugParams>,
+  res: Response,
+): Promise<void> {
+  const { slug } = req.params;
+  const product = await productService.getProduct(slug);
+  if (!product) {
+    throw new ApiError(404, "El producto no existe");
+  }
+  res.json({ status: "success", data: product });
 }
